c102106328f1c1d398e3ea408f6eaaf554476d14
BenIsRong <benisrong@gmail.com>
2025-02-19 15:47:22 +0800

    feat: trigger event from another controller

--- papi/traits/listenable.php
+++ papi/traits/listenable.php
@@ -12,6 +12,20 @@ trait Listenable
         $model = str_replace("Controller", "", $model);
         $event = "\\events\\" . $model . "Event";
 
-        $event::$function($model, $function);
+        $event::$function($model, $function, $function);
+    }
+
+    public function triggers(string $model, string $function = "")
+    {
+        $callFunction = debug_backtrace()[1]['function'];
+        if ($function == "") {
+            $function = $callFunction;
+        }
+
+        $model = explode("\\", $model);
+        $model = end($model);
+        $event = "\\events\\" . $model . "Event";
+
+        $event::$function($model, $function, $callFunction);
     }
 }
